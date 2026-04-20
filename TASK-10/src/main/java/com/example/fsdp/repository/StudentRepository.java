95cb36be7d02590246d742d63d59618f4193e670
Midakanti Sudeepthi <253428348+midakantisudeepthi@users.noreply.github.com>
2026-04-20 12:32:48 +0530

    Delete TASK-10 directory

--- TASK-10/src/main/java/com/example/fsdp/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fsdp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.fsdp.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
